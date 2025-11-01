f692a42742c506c25d2b50591edd9a95d4cb1d68
kouei <kagigokoro@gmail.com>
2025-11-02 06:27:30 +0800

    Update example12.cpp

--- main.cpp
+++ main.cpp
@@ -18,11 +18,18 @@ template <auto Val>
   requires(isPrime(Val))
 class C1 {};
 
-template<auto Val>
+template <auto Val>
 concept IsPrime = Val > 0 && isPrime(Val);
+
+template <auto Val>
+  requires IsPrime<Val>
+class C2 {};
 
 int main() {
   // auto c1 = C1<6>(); // Error
   // auto c2 = C1<7>(); // OK
+
+  // auto c1 = C2<6>(); // Error
+  // auto c2 = C2<7>(); // OK
   return 0;
 }
