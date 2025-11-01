ebf52cdc62e5cf87ee1b1b54fa514427de6d1587
kouei <kagigokoro@gmail.com>
2025-11-02 06:24:24 +0800

    Update example12.cpp

--- archive/example12.cpp
+++ archive/example12.cpp
@@ -18,6 +18,9 @@ template <auto Val>
   requires(isPrime(Val))
 class C1 {};
 
+template<auto Val>
+concept IsPrime = Val > 0 && isPrime(Val);
+
 int main() {
   // auto c1 = C1<6>(); // Error
   // auto c2 = C1<7>(); // OK
--- main.cpp
+++ main.cpp
@@ -2,40 +2,27 @@
 #include <list>
 #include <vector>
 
-template <typename T> class MyType {
-private:
-  T value;
+constexpr bool isPrime(int val) {
+  if (val <= 1)
+    return false;
 
-public:
-  MyType(const T &val) : value(val) {}
-
-  void print() const { std::cout << value << "\n"; }
-
-  bool isZero() const
-    requires std::integral<T> || std::floating_point<T>
-  {
-    return value == 0;
+  for (int i = 2; i <= val / 2; ++i) {
+    if (val % i == 0)
+      return false;
   }
 
-  bool isEmpty() const
-    requires(requires { value.empty(); })
-  {
-    return value.empty();
-  }
-};
+  return true;
+}
+
+template <auto Val>
+  requires(isPrime(Val))
+class C1 {};
+
+template<auto Val>
+concept IsPrime = Val > 0 && isPrime(Val);
 
 int main() {
-  auto mt1 = MyType<double>{3.14};
-
-  mt1.print(); // OK
-  // if (mt1.isZero()) // OK
-  // if (mt1.isEmpty()) // Error
-
-  auto mt2 = MyType<std::string>{"abc"};
-
-  mt2.print(); // OK
-  // if(mt2.isZero()) // Error
-  // if(mt2.isEmpty()) // OK
-
+  // auto c1 = C1<6>(); // Error
+  // auto c2 = C1<7>(); // OK
   return 0;
 }
