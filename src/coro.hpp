8e878907db54e7f3b3e2738ff5872d5be5b04635
kouei <kagigokoro@gmail.com>
2025-11-06 11:14:14 +0800

    Update src

--- src/coro.hpp
+++ src/coro.hpp
@@ -1,14 +1,3 @@
-//********************************************************
-// The following code example is taken from the book
-//  C++20 - The Complete Guide
-//  by Nicolai M. Josuttis (www.josuttis.com)
-//  http://www.cppstd20.com
-//
-// The code is licensed under a
-//  Creative Commons Attribution 4.0 International License
-//  http://creativecommons.org/licenses/by/4.0/
-//********************************************************
-
 #include "corotask.hpp" // for CoroTask
 #include <iostream>
 
